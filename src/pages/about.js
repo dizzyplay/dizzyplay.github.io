fcf703400cab373bc78133f5ea79528d27919b92
dizzyplay <vovoboss@gmail.com>
2020-03-20 00:20:38 +0900

    Updates

--- src/pages/about.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import {Link} from "gatsby"
-
-export default () => {
-  return <div> about me<br/><Link to={'/'}>index</Link></div>
-}
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import {Link} from "gatsby"
-
-export default () => <div style={{ color: "purple", fontSize: "72px" }}>Hello world!
-  <Link to={'about'}> about </Link>
-</div>
